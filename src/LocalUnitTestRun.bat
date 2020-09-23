3daff1a878b7a4c2ed313411d51e921a9f6b4305
Matt Whited <matt,whited@lightwellinc.com>
2020-09-23 17:38:53 -0400

    update and test

--- src/LocalUnitTestRun.bat
+++ src/LocalUnitTestRun.bat
@@ -4,23 +4,40 @@
 
 SET TestProject=BinaryDataDecoders.sln
 SET Configuration=Debug
+SET OutputPath=Publish
 
-REM VisualStudioVersion=16.0
-REM              VSAPPIDDIR=C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\
-REM SET SQLDBExtensionsRefPath=%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Microsoft\VisualStudio\v16.0\SSDT
 SET SQLDBExtensionsRefPath=%VSAPPIDDIR%\..\..\MSBuild\Microsoft\VisualStudio\v%VisualStudioVersion%\SSDT
 
+echo "Install Report Generator"
 dotnet tool install --global dotnet-reportgenerator-globaltool 2>NUL
+
 rmdir /s/q ".\TestResults"
-REM /Coverage/Reports
-dotnet test %TestProject% --configuration %Configuration% --collect:"XPlat Code Coverage" -r .\TestResults --nologo --filter "TestCategory=Unit|TestCategory=Simulate" -s .runsettings
+rmdir /s/q "%OutputPath%"
+
+echo "Git fetch"
+git fetch --prune
+
+echo "Clean Packages"
+dotnet clean "%TestProject%"
+
+echo "Restore Packages"
+dotnet restore "%TestProject%""
+
+echo "Build Packages"
+dotnet build "%TestProject%" --configuration %Configuration% --no-restore
+
+echo "Run Tests"
+dotnet test "%TestProject%" --no-build --no-restore --collect:"XPlat Code Coverage" -r .\TestResults --nologo --filter "TestCategory=Unit|TestCategory=Simulate" -s .runsettings
 SET TEST_ERR=%ERRORLEVEL%
 reportgenerator "-reports:.\TestResults\**\coverage.cobertura.xml" "-targetDir:.\TestResults\Coverage\Reports" -reportTypes:HtmlInline;Cobertura "-title:%TestProject% - (%USERNAME%)"
 start ./TestResults/Coverage/Reports\index.html
 
+echo "Pack Projects"
+dotnet pack --no-build --no-restore "%TestProject%"  -o "%OutputPath%/Nuget"
+
 ECHO TEST_ERR=%TEST_ERR%
 IF %TEST_ERR%==0 (
 	ECHO "No Errors :)"
-	EXIT 0
+	REM EXIT 0
 )
 PAUSE
