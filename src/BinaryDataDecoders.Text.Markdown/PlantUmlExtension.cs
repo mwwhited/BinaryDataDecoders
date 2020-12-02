d618d57dc04b9f0e83bef7c095b310cc51e0c821
Matthew Whited <matt@whited.us>
2020-12-02 18:46:56 -0500

    start build out for converting plantuml to images with collaspe support on github

---
+++ src/BinaryDataDecoders.Text.Markdown/PlantUmlExtension.cs
@@ -0,0 +1,24 @@
+﻿using Markdig;
+using Markdig.Renderers;
+using Markdig.Renderers.Normalize;
+
+namespace BinaryDataDecoders.Text.Markdown
+{
+    public class PlantUmlExtension : IMarkdownExtension
+    {
+        public void Setup(MarkdownPipelineBuilder pipeline)
+        {
+            if (!pipeline.BlockParsers.Contains<PlantUmlBlockParser>())
+                pipeline.BlockParsers.Insert(0, new PlantUmlBlockParser());
+        }
+
+        public void Setup(MarkdownPipeline pipeline, IMarkdownRenderer renderer)
+        {
+            if (renderer is HtmlRenderer html && !html.ObjectRenderers.Contains<PlantUmlHtmlBlockRenderer>())
+                html.ObjectRenderers.Insert(0, new PlantUmlHtmlBlockRenderer(pipeline));
+
+            if (renderer is NormalizeRenderer github && !github.ObjectRenderers.Contains<PlantUmlGithubMarkdownBlockRenderer>())
+                github.ObjectRenderers.Insert(0, new PlantUmlGithubMarkdownBlockRenderer(pipeline));
+        }
+    }
+}
