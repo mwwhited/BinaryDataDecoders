8f32e4795eb95ed2814b167139c22109c2a6e540
Matthew Whited <matt@whited.us>
2022-10-26 09:46:41 -0400

    Update XFragment.cs

--- src/BinaryDataDecoders.ToolKit/Xml/Linq/XFragment.cs
+++ src/BinaryDataDecoders.ToolKit/Xml/Linq/XFragment.cs
@@ -11,11 +11,12 @@ namespace BinaryDataDecoders.ToolKit.Xml.Linq
 
     public class XFragment : IList<XNode>
     {
+        // https://github.com/OutOfBandDevelopment/Samples/blob/master/HandyClasses/XFragment.cs
         private IList<XNode> Nodes { get; } = new List<XNode>();
 
         public XFragment(IEnumerable<XNode> nodes)
         {
-            foreach (var node in nodes ?? Enumerable.Empty<XNode>().Where(n => n != null))
+            foreach (var node in (nodes ?? Enumerable.Empty<XNode>()).Where(n => n != null))
                 this.Nodes.Add(node);
         }
 
@@ -24,16 +25,16 @@ namespace BinaryDataDecoders.ToolKit.Xml.Linq
         {
         }
 
-        public XFragment(string xml)
-            : this(XFragment.Parser(xml).ToArray())
+        public XFragment(string? xml)
+            : this(Parser(xml).ToArray())
         {
         }
         public XFragment(XmlReader xmlReader)
-            : this(XFragment.Parser(xmlReader).ToArray())
+            : this(Parser(xmlReader).ToArray())
         {
         }
 
-        private static IEnumerable<XNode> Parser(string xml)
+        private static IEnumerable<XNode> Parser(string? xml)
         {
             if (string.IsNullOrWhiteSpace(xml))
                 yield break;
@@ -46,8 +47,10 @@ namespace BinaryDataDecoders.ToolKit.Xml.Linq
 
             using (var stringReader = new StringReader(xml))
             using (var xmlReader = XmlReader.Create(stringReader, settings))
-                foreach (var node in Parser(xmlReader))
+            {
+                foreach (var node in XFragment.Parser(xmlReader))
                     yield return node;
+            }
         }
 
         private static IEnumerable<XNode> Parser(XmlReader xmlReader)
@@ -60,50 +63,49 @@ namespace BinaryDataDecoders.ToolKit.Xml.Linq
                 yield return XNode.ReadFrom(xmlReader);
         }
 
-        public override string ToString() => (string)this;
-        public XmlReader CreateReader() =>
-            XmlReader.Create(new StringReader(this), new XmlReaderSettings
-            {
-                ConformanceLevel = ConformanceLevel.Fragment,
-            });
+        public override string ToString() => this;
+
+        public XmlReader CreateReader() => XmlReader.Create(new StringReader(this), new XmlReaderSettings
+        {
+            ConformanceLevel = ConformanceLevel.Fragment,
+        });
 
         public static XFragment Parse(string xml) => new XFragment(xml);
         public static XFragment Parse(XmlReader xmlReader) => new XFragment(xmlReader);
 
         #region IEnumerable 
 
-        public IEnumerator<XNode> GetEnumerator() =>
-            (this.Nodes ?? Enumerable.Empty<XNode>()).Where(n => n != null).GetEnumerator();
-
-        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+        public IEnumerator<XNode> GetEnumerator() => (Nodes ?? Enumerable.Empty<XNode>()).Where(n => n != null).GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
         #endregion
 
         #region IList
 
-        public int Count => this.Nodes.Count;
-        public bool IsReadOnly => this.Nodes.IsReadOnly;
+        public int Count => Nodes.Count;
+        public bool IsReadOnly => Nodes.IsReadOnly;
         public XNode this[int index]
         {
-            get => this.Nodes[index];
-            set => this.Nodes[index] = value;
+            get => Nodes[index];
+            set => Nodes[index] = value;
         }
 
-        public int IndexOf(XNode item) => this.Nodes.IndexOf(item);
-        public void Insert(int index, XNode item) => this.Nodes.Insert(index, item);
-        public void RemoveAt(int index) => this.Nodes.RemoveAt(index);
-        public void Add(XNode item) => this.Nodes.Add(item);
-        public void Clear() => this.Nodes.Clear();
-        public bool Contains(XNode item) => this.Nodes.Contains(item);
-        public void CopyTo(XNode[] array, int arrayIndex) => this.Nodes.CopyTo(array, arrayIndex);
-        public bool Remove(XNode item) => this.Nodes.Remove(item);
+        public int IndexOf(XNode item) => Nodes.IndexOf(item);
+        public void Insert(int index, XNode item) => Nodes.Insert(index, item);
+        public void RemoveAt(int index) => Nodes.RemoveAt(index);
+        public void Add(XNode item) => Nodes.Add(item);
+        public void Clear() => Nodes.Clear();
+        public bool Contains(XNode item) => Nodes.Contains(item);
+        public void CopyTo(XNode[] array, int arrayIndex) => Nodes.CopyTo(array, arrayIndex);
+        public bool Remove(XNode item) => Nodes.Remove(item);
 
         #endregion
 
         #region Conversions 
 
-        public static implicit operator XFragment(string xml) => new XFragment(xml);
-        public static implicit operator string(XFragment fragment)
+        public static implicit operator XFragment(string? xml) => new XFragment(xml);
+
+        public static implicit operator string?(XFragment fragment)
         {
             if (fragment == null)
                 return null;
@@ -115,17 +117,18 @@ namespace BinaryDataDecoders.ToolKit.Xml.Linq
             };
             var sb = new StringBuilder();
             using (var xmlwriter = XmlWriter.Create(sb, settings))
+            {
                 foreach (var node in fragment)
                 {
-                    node.WriteTo(xmlwriter);
-                    //var reader = node.CreateReader();
-                    //    xmlwriter.WriteNode(reader, false);
+                    xmlwriter.WriteNode(node.CreateReader(), false);
                 }
+            }
 
             return sb.ToString();
         }
 
         public static implicit operator XFragment(XNode[] nodes) => new XFragment(nodes);
+
         public static implicit operator XFragment(XNode node) => new XFragment(node);
 
         #endregion
