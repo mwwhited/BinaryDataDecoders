28f30b5dd6db4dd39bb0ef57eb74ae992d738d06
Matthew Whited <matt@whited.us>
2023-05-20 13:09:32 -0400

    Add new functions to toolkit

---
+++ src/BinaryDataDecoders.ToolKit/Linq/TupleExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace BinaryDataDecoders.ToolKit.Linq
+{
+    public static class TupleExtensions
+    {
+        public static object[] ToArray(this ITuple tuple) =>
+            Enumerable.Range(0, tuple.Length).Select(i => tuple[i]).ToArray();
+        public static T[] ToArray<T>(this ITuple tuple) =>
+            Enumerable.Range(0, tuple.Length).Select(i => (T)tuple[i]).ToArray();
+        public static IReadOnlyList<T> ToList<T>(this ITuple tuple) =>
+            Enumerable.Range(0, tuple.Length).Select(i => (T)tuple[i]).ToArray();
+    }
+}
